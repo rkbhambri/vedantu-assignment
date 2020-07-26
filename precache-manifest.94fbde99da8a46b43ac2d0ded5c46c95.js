ff230a5c99f8516b5c2217b7fe263a4fd59781ea
rkbhambri <ramkrishan@redbaron.in>
2020-07-26 11:09:39 +0530

    Updates

---
+++ precache-manifest.94fbde99da8a46b43ac2d0ded5c46c95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445f23abc99aa2eb6a904364bc27e85f",
+    "url": "/vedantu-assignment/index.html"
+  },
+  {
+    "revision": "d8fe3b8acfe6bce76693",
+    "url": "/vedantu-assignment/static/css/main.0e55862f.chunk.css"
+  },
+  {
+    "revision": "b5d2c79deb0ac3c3eab2",
+    "url": "/vedantu-assignment/static/js/2.b401ba71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vedantu-assignment/static/js/2.b401ba71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8fe3b8acfe6bce76693",
+    "url": "/vedantu-assignment/static/js/main.5157cc4a.chunk.js"
+  },
+  {
+    "revision": "d3ceb9e165ecf8d5e529",
+    "url": "/vedantu-assignment/static/js/runtime-main.e6f02b88.js"
+  }
+]);
